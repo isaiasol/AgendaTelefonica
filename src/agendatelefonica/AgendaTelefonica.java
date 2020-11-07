a36fe1b72a3e59ad83c92b703c97d79bf09d00bc
Isaias Ortiz <isaias@huauchinango.tecnm.mx>
2020-11-07 12:29:40 -0600

    Segunda version de agenda

--- src/agendatelefonica/AgendaTelefonica.java
+++ src/agendatelefonica/AgendaTelefonica.java
@@ -5,6 +5,8 @@
  */
 package agendatelefonica;
 
+import java.util.Scanner;
+
 /**
  *
  * @author OrtizL
@@ -15,6 +17,25 @@ public class AgendaTelefonica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String nombre;
+        String apellidoP;
+        String apellidoM;
+        String NumTelefono;
+        Scanner teclado=new Scanner(System.in);
+        
+        System.out.println("Escribe tu nombre: ");
+        nombre=teclado.nextLine();
+        
+        System.out.println("Escribe tu apellido paterno: ");
+        apellidoP=teclado.nextLine();
+        
+        System.out.println("Escribe tu apellido materno: ");
+        apellidoM=teclado.nextLine();
+        
+        System.out.println("Escribe tu número telefónico: ");
+        NumTelefono=teclado.nextLine();
+        
+        System.out.println("El contacto " + nombre + apellidoP + apellidoM + NumTelefono + "ha quedado registrado");
         // TODO code application logic here
     }
     
